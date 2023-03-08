9c3ad5aa93362780781c60a2ebad90f61ddc6a5d
emer2006 <126901936+emer2006@users.noreply.github.com>
2023-03-08 00:17:51 -0500

    Update gc-tagall.js

--- comandos/gc-tagall.js
+++ comandos/gc-tagall.js
@@ -5,10 +5,10 @@ throw false
 }
 let pesan = args.join` `
 let oi = `*▪𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`
-let teks = `*⺀𝐀𝐂𝐓𝐈𝐕𝐄𝐍𝐒𝐄 𝐁𝐎𝐓𝐒 - 𝐁𝐨𝐭𝐉𝐞𝐧𝐧𝐱𝐓𝐚𝐭𝐢⺀*\n\n▪ ${oi}\n\n▪ *𝐄𝐭𝐢𝐪𝐮𝐞𝐭𝐚 𝐠𝐞𝐧𝐞𝐫𝐚𝐥:*\n`
+let teks = `*⺀𝐀𝐂𝐓𝐈𝐕𝐄𝐍𝐒𝐄 𝐁𝐎𝐓𝐒 - CHARLIEBOT2.0⺀*\n\n▪ ${oi}\n\n▪ *𝐄𝐭𝐢𝐪𝐮𝐞𝐭𝐚 𝐠𝐞𝐧𝐞𝐫𝐚𝐥:*\n`
 for (let mem of participants) {
-teks += `┣✉️ @${mem.id.split('@')[0]}\n`}
-teks += `└𝐉𝐞𝐧𝐧𝐱𝐓𝐚𝐭𝐢`
+teks += `┣💌 @${mem.id.split('@')[0]}\n`}
+teks += `└@emx._xit`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
